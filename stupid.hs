aa9a09f1b38dab44bd2801dc6be88cf64baf609b
Bryan Richter <b@chreekat.net>
2014-10-06 15:24:23 -0700

    Rough draft rest of game logic

--- stupid.hs
+++ stupid.hs
@@ -1,5 +1,5 @@
 import System.Environment (getArgs)
-import Control.Applicative ((<$>))
+import Control.Applicative ((<$>), (<*>))
 import Control.Error
 import Control.Monad ((<=<))
 import Control.Monad.Free
@@ -33,10 +33,52 @@ passStage = do
     r <- attackRank
     card <- minimumMay <$> (cardsRanked r Defense)
     case (compare nOff nTable, card) of
+        -- offense has enough cards && defense has one to play
         (GT, Just card'@(Card s r')) -> do
             playCard Defense card'
             swapRoles
             passStage
+        -- else...
         _ -> defendStage
 
-defendStage = return $ Pure ()
+defendStage = do
+    mTarget <- minimumMay <$> uncoveredCards
+    success <- coverCard' mTarget
+    case (mTarget, success) of
+        -- There was a card, it was defended
+        (Just _, Just True) -> defendStage
+        -- No card OR the card could not be defended against
+        _                   -> reinforceStage
+
+  where
+
+    coverCard' :: Maybe Card -> GameDSL (Maybe Bool)
+    coverCard' mTarget = case mTarget of
+        Just target -> Just <$> coverCard target
+        Nothing     -> return $ Nothing
+
+reinforceStage = do
+    nUnc <- length <$> uncoveredCards
+    nDef <- length <$> getHand Defense
+    reinforcements <- (take (nDef - nUnc)) <$> possibleReinforcements
+
+    case reinforcements of
+        [] -> verdictStage nUnc
+        _  -> do
+            reinforceWith reinforcements
+            defendStage
+
+verdictStage numUncovered = do
+    case numUncovered of
+        0 -> winTurn Defense
+        _ -> winTurn Offense
+
+    (oHand, dHand) <- (,)
+        <$> getHand Offense
+        <*> getHand Defense
+
+    case (oHand, dHand) of
+        ([], []) -> tieGame
+        (_ , []) -> winner Defense
+        ([],  _) -> winner Offense
+        _        -> attackStage
